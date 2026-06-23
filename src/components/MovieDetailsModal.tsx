57efca484d56d19a85555dae66427a73207d1da1
JayaSree <js@eventdesk.co>
2026-06-23 11:33:33 -0700

    modal css

--- src/components/MovieDetailsModal.tsx
+++ src/components/MovieDetailsModal.tsx
@@ -40,44 +40,42 @@ export default function MovieDetailsModal({
             </div>
 
             <div style={styles.info}>
-              <div>
-                <img
-                  style={styles.image}
-                  src={movie.Poster || placeholderSrc}
-                  alt={movie.Title}
-                  onError={(event) => {
-                    event.currentTarget.src = placeholderSrc;
-                  }}
-                />
+              <img
+                style={styles.image}
+                src={movie.Poster || placeholderSrc}
+                alt={movie.Title}
+                onError={(event) => {
+                  event.currentTarget.src = placeholderSrc;
+                }}
+              />
 
-                <div
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  gap: '15px',
+                }}
+              >
+                <h3
                   style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    gap: '15px',
+                    overflowWrap: 'break-word',
                   }}
                 >
-                  <h3
-                    style={{
-                      overflowWrap: 'break-word',
-                    }}
-                  >
-                    {movie.Title}
-                  </h3>
-                  <div style={{ display: 'flex', gap: '20px' }}>
-                    <label>{movie.Year}</label>
-                    {''}
-                    <label>{movie.Genre}</label>
-                    {''}
-                    <label>{movie.Runtime}</label>
-                    {''}
-                    <label>{movie.imdbRating}</label>
-                    {''}
-                  </div>
-                  <div>
-                    <label>{movie.Plot}</label>
-                  </div>
+                  {movie.Title}
+                </h3>
+                <div style={{ display: 'flex', gap: '20px' }}>
+                  <label>{movie.Year}</label>
+                  {''}
+                  <label>{movie.Genre}</label>
+                  {''}
+                  <label>{movie.Runtime}</label>
+                  {''}
+                  <label>{movie.imdbRating}</label>
+                  {''}
+                </div>
+                <div>
+                  <label>{movie.Plot}</label>
                 </div>
               </div>
             </div>
@@ -121,13 +119,16 @@ const styles: Record<string, React.CSSProperties> = {
     wordBreak: 'break-word',
   },
   info: {
+    display: 'flex',
+    flexDirection: 'column',
     borderRadius: '12px',
-    padding: '12px',
     overflow: 'hidden',
+    alignItems: 'center',
+    gap: '15px',
   },
   image: {
     borderRadius: '8px',
-    marginBottom: '5px',
+    margin: '5px',
     maxWidth: '35%',
     height: 'auto',
   },
